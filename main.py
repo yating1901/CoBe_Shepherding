ae9f8927c0a5abcb45c34c7c4617f3961ba49978
yating1901 <zhengyating@mail.bnu.edu.cn>
2024-08-29 14:06:16 +0200

    update setup in main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 Target_place_x = 800
 Target_place_y = 800
-Target_size = 100  # radius
+Target_size = 200  # radius
 
 Boundary_x = Target_place_x + Target_size
 Boundary_y = Target_place_y + Target_size
@@ -9,11 +9,11 @@ Boundary_y = Target_place_y + Target_size
 TICK = 1
 Iterations = 10000
 
-N_sheep = 10
+N_sheep = 100
 N_shepherd = 3
-L3 = 100
+L3 = 50
 
-Robot_Loop = True
+Robot_Loop = False
 
 from loop_function import Loop_Function
 
@@ -25,6 +25,9 @@ loop_function = Loop_Function(N_sheep=N_sheep,  # Number of agents in the enviro
                               target_place_x = Target_place_x,
                               target_place_y = Target_place_y,
                               target_size = Target_size,
+                              framerate=50,
+                              window_pad=30,
+                              with_visualization=True,
                               agent_radius=10, # Agent radius in pixels
                               L3 = L3,  # repulsion distance
                               robot_loop = Robot_Loop)
